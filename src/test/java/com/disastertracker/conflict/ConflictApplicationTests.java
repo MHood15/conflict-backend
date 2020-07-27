05e343644b023da0a1fa217aed5e1d9bdec58b29
MHood15 <mhood1598@gmail.com>
2020-07-27 07:06:03 -0600

    Initial commit

---
+++ src/test/java/com/disastertracker/conflict/ConflictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disastertracker.conflict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConflictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
